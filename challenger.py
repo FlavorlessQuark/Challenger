d88be96a74dca35349d894c42caf22ab5c76f886
FlavorlessQuark <flavorlessquark@gmail.com>
2021-05-08 01:10:53 -0700

    Did not expect the unexpected. Captcha time :clock:

--- challenger.py
+++ challenger.py
@@ -2,19 +2,24 @@ import os
 import re
 from srcs import setup
 
-# Check if config file exist, else run setup
 
-with open ("srcs/challenger.config") as f:
-	config = {line.split(":")[0]:line.split(",")[0:] for line in  f }
+compile_lines = {
+					".c": "gcc -g main.c && ./a.out",
+					".py":"python3 main.py"
 
-print(config)
-print("Welcome user")
-print("Exercises are available from the following: Leetcode, Advent of Code, Project Euler.")
+}
 
-while True:
-	answer = input()
+commands = {
+				"end":"save()",
+				"test":"test()",
+				"start":"start(answer)",
+				"pause":"pause()",
+				"submit":"submit()"
+}
+
+def start(answer):
 	match = re.match(r'^start (leetcode|aoc|euler) (\..+)$', answer)
-	if match and config[match.groups(0)[0]][0] != "":
+	if match and config[match.groups()[0]]:
 		print("workspace")
 		setup.setup_workspace(match.groups())
 		break
@@ -26,3 +31,25 @@ while True:
 	else:
 		print("Invalid optiions. Usage: start website language_extension")
 
+
+# ----------------------------------------------------------------#
+# Check if config file exist, else run setup
+if not os.path.dirname("challenger.config"):
+	with open("challenger.config", 'w+') as f:
+			f.write("leetcode:\naoc:\neuler:\n")
+
+with open ("challenger.config") as f:
+	config = {line.split(":")[0]:line.strip().split(",")[1:] for line in  f }
+
+
+print(">> Welcome user")
+print(">> Exercises are available from the following: Leetcode (leetcode) | Advent of Code (aoc) | Project Euler (euler)")
+
+# Setup the workspace and add information necessary to save files
+
+while True:
+	answer = input().split(" ")
+	if answer[0] not in commands:
+		print(">? Invalid command: " + answer[0] + "| Available commands are : start, pause, finish")
+	else:
+		eval(commands[answer[0]])
--- srcs/setup.py
+++ srcs/setup.py
@@ -1,29 +1,30 @@
 import os
-import subprocess
+from fileinput import FileInput as fp
 
 def setup_website(sitename):
-	directory = ""
-
-	print("Now setting up", sitename, "Please enter thhe directory where you would like your files to be saved")
+	
+	print(">> Now setting up", sitename, "Please enter the directory where you would like your files to be saved")
+	directory = input()
 	while not os.path.dirname(directory):
 		print("Please enter a valid directory")
 		directory = input()
 
-	url = "none"
-	if os.path.dirname(directory + "/.git"):
-		os.chdir(directory)
-		url = subprocess.check_output("git remote show origin | grep \"Fetch URL\" | sed \'s/ *Fetch URL: //\'")
-	return [directory, url, ""]
-
+	git = True if os.path.dirname(directory + "/.git") else False
+	with fp("challenger.config", inplace = True) as f:
+		for line in f:
+			if line.startswith(sitename):
+				print(sitename + ":" + directory + "," + str(git), end="\n")
+			else:
+				print(line, end="")
+	return directory
 
 def setup_workspace(data):
 	if not os.path.dirname("Workspace"):
 		os.mkdir("Workspace")
 	if data[0] != "euler":
-		with open("Workspace/input", 'w') as f:
+		with open("Workspace/input", 'w+') as f:
 			pass
-	with open("Workspace/main" + data[1], 'w') as f:
+	with open("Workspace/main" + data[1], 'w+') as f:
 		pass
-	with open("Workspace/prompt.txt", 'w') as f:
+	with open("Workspace/prompt.txt", 'w+') as f:
 		pass
-
