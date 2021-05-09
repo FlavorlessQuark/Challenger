bdfa9c0db31dc52c36b41b2a41ce27c502389572
FlavorlessQuark <flavorlessquark@gmail.com>
2021-05-09 05:31:53 -0700

    Euler and aoc

--- srcs/login.py
+++ srcs/login.py
@@ -2,10 +2,11 @@ from requests import Session
 from bs4 import BeautifulSoup as bs
 import base64
 
+
 # class coding_session:
 # 	def __init__(self, site, url):
 # 		self.url = url
-# 		self.cookies = {"PHPSESSID": "1d6b619ffc3c085c1005994454c0e593"}
+# 		self.cookies = {"PHPSESSID": "Nope"}
 # 		self.session = requests.Session()
 
 # 	def get_problem(self, problem_no):
@@ -16,21 +17,25 @@ import base64
 # 	#Check cookies for expiration
 url = "https://projecteuler.net/problem=3"
 
+# cookies = {
+# 	"session": "<.< MY token"
+# }
 cookies = {
-	"PHPSESSID": "1d6b619ffc3c085c1005994454c0e593"
+	"PHPSESSID": "look elsewehre"
 }
 
 with Session() as session:
 	response = session.get(url, cookies=cookies)
 	html = bs(response.content, "html.parser")
 	token = html.find("input", {"name":"csrf_token"})["value"]
-	payload = {"guess_3":"42", "csrf_token":token}
-	# soup = bs(html, "lxml")
-	response = session.post(url, data=payload, cookies=cookies, )
+	payload = {"guess_3":"6857", "csrf_token":token}
+	# # # soup = bs(html, "lxml")
+	response = session.post(url + "/problem=3", data=payload, cookies=cookies)
 	html = bs(response.content, "html.parser")
-	# table = html.find("div",{"class": "problem_content"})
-	table = html.find("img",{"title": "wrong"})
-	print(table)
+	# action = html.find("form})
+	print(html.prettify())
+	# # table = html.find("div",{"class": "problem_content"})
+	# table = html.find("img",{"title": "wrong"})
 	# for x in table:
 	# 	print(x)
 
