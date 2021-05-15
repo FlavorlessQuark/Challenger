ffd0dc132f2f95fdd982791556e7e22fc023ef3f
FlavorlessQuark <flavorlessquark@gmail.com>
2021-05-14 19:23:58 -0700

    Cleanup

--- bin/code_sessions.py
+++ bin/code_sessions.py
@@ -41,30 +41,3 @@ class euler_session:
 			return False
 		return True
 	#  Code below
-
-# url = "https://projecteuler.net/problem=4"f3931a3093972279f099afebaa5a10d6
-# cookies = {"PHPSESSID":"1d6b619ffc3c085c1005994454c0e593"}
-# 	def save():
-
-# with Session() as session:
-# 	response = session.get(url, cookies=cookies)
-# 	html = bs(response.content, "html.parser")
-# 	token = html.findAll("input", {"name":"csrf_token"})[-1]["value"]
-# 	payload = {"guess_4":"3", "csrf_token":token}
-# 	# 42de3a0f57a4dfbca0e8ad629d1e49d01lZHtQUOMQrloqilLnznXBEKGJvXvxCj8Vpl+TzoDC29mXgsFC748hrVQhQO9S53bq5FeKIfOeM4X2ie
-# 	# 1895d1367d4df4489267a6ea498e78caBI16mnM+mqy1tHySOJ0rjxb0Rj4Jiw/NcGcqsoqsLJMPuauAm2UR0TbWuG/Ym7VDiz2lYpzJOL0hkL2mp7HMoHyjyw==
-# # 	# # # soup = bs(html, "lxml")
-# 	response = session.post(url, data=payload, cookies=cookies)
-# 	html = bs(response.content, "html.parser")
-# # 	# action = html.find("form})
-# 	# print(token)
-# 	print(html.prettify())
-# 	# # table = html.find("div",{"class": "problem_content"})
-# 	# table = html.find("img",{"title": "wrong"})
-# 	# for x in table:
-# 	# 	print(x)
-
-# 	# # print(response.text)
-# 	# # print(html.find("div", {"class": "problem_content"}))
-# 	# # response = session.get(url)
-# 	# # print(html)
